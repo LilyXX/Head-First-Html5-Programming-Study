c9a1ceec0eb33aa274a13ac0b94f42665b71b863
LilyXX <843989276@qq.com>
2017-02-20 17:33:14 +0800

    最近学习

---
+++ Haversine.js
@@ -0,0 +1,16 @@
+       function computeDistance(startCoords,destCoords){
+            var startLatRads = degreesToRadians(startCoords.latitude);
+            var startLongRads = degreesToRadians(startCoords.longitude);
+            var destLatRads = degreesToRadians(destCoords.latitude);
+            var destLongRads = degreesToRadians(destCoords.longitude);
+
+            var Radius = 6371; // Radius of the Earth in km
+            var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) +　Math.cos(startLatRads) * Math.cos(destLatRads) * Math.cos(startLongRads = destLongRads)) * Radius;
+
+            return distance;
+        }
+
+        function degreesToRadians(degrees){
+            var radians = (degrees * Math.PI)/180;
+            return radians;
+        }
---
+++ canvas/tweetshiirt.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+            var canvas = document.getElementById("tshirtCanvas");
+            var context = canvas.getContext("2d");
+            context.fillRect(10,10,100,100);
+        }
+window.onload = function(){
+    var button = document.getElementById("previewButton");
+}
